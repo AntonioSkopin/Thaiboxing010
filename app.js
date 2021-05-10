3e35ae226ed90c9ac71dd99c818a04bb3997224f
AntonioSkopin <askopin@outlook.com>
2021-05-10 08:41:30 +0200

    Rebuilt landing page

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ window.onscroll = () => {
 
     // When user scrolls add shadow to navbar
     if (window.scrollY > 0) {
-        nav.classList.add("shadow-lg");
+        nav.classList.add("shadow-md");
+        nav.classList.add("bg-white");
     } else {
-        nav.classList.remove("shadow-lg");
+        nav.classList.remove("shadow-md");
+        nav.classList.remove("bg-white");
     }
 }
